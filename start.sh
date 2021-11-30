1b292f8b2343d5fa29c3877d2d8b2084ae2acd00
ngonhan2k5 <ngonhan2k5@gmail.com>
2021-11-30 10:55:03 +0000

    add start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,44 @@
-if [ -d dist ]; then
-    npm start
-else
+#!/bin/bash
+APP=blog
+
+start(){
+    nohup npm start > $APP.log 2>&1 &
+    echo $! > pid.txt
+}
+
+build(){
     npm run buildProd
-    npm start
-fi
+}
+
+stop(){
+    kill -9 `cat pid.txt`
+    rm pid.txt
+}
+
+case "$1" in
+  install)
+    install $2
+    ;;
+  build)
+    build
+    ;;
+  start)
+    start
+    ;;
+  stop)
+    stop
+    ;;
+  uninstall)
+    uninstall
+    ;;
+  retart)
+    stop
+    start
+    ;;
+  *)
+    if "$1"; then
+      echo "Success $1"
+    else
+      echo "Usage: $0 {start|stop|restart|uninstall}"
+    fi
+esac
